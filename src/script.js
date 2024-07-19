674231ad4b3a2e9e72e951005b48873c79f49014
mahlelulu <168305119+mahlelulu@users.noreply.github.com>
2024-07-19 22:49:56 +0200

    added more weather info

--- src/script.js
+++ src/script.js
@@ -5,31 +5,42 @@ function displayTemperature(response){
     let descriptionElement = document.querySelector("#description");
     let humidityInput=document.querySelector("#humidity");
     let windSpeedInput=document.querySelector("#wind-speed");
+    let timeInput=document.querySelector("#time");
+    let date= new Date(response.date.time*1000);
+
     TemperatureInput.innerHTML=temperature;
     descriptionElement.innerHTML=response.data.condition.description;
     cityInput.innerHTML=response.data.city;
     humidityInput.innerHTML=`${response.data.temperature.humidity}%`;
-    windSpeedInput.innerHTML= response.data.temperature.wind.speed;
-
+    windSpeedInput.innerHTML= `${response.data.temperature.wind.speed}km/h`;
+    timeInput.innerHTML= formatDate(date);
 }
-function enterCity(event) {
-  event.preventDefault();
-  let inputType = document.querySelector("#search-input");
-  let city=inputType.value
-  let apiKey= "ab4d0edb5b9372141t1b46d08bocdf06"
-  let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
- axios.get(apiUrl).then(displayTemperature)
-
-}
-   
-    function formatDay(date){
-    let day=date.getDay();
+function formatDate(date){
     let days=["sunday","monday","tuesday","wednesday","thursday","friday","saturday" ]
-    let formattedDay = days[day];
     let hour=date.getHours();
     let minute= date.getMinutes();
-    return `${formattedDay} ${hour}:${minute}, `
+    let day =days[date.getDay()];
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
     }
+    return `${day} ${hour}:${minute}, `
+    }
+
+
+   function searchCity(city){
+    let apiKey= "ab4d0edb5b9372141t1b46d08bocdf06"
+  let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+ axios.get(apiUrl).then(displayTemperature);
+
+ 
+function enterCity(event) {
+    event.preventDefault();
+    let inputType = document.querySelector("#search-input");
+    searchCity(inputType.value)
+    
+  }
+   }
+    
     let country = document.querySelector("#form-search");
     country.addEventListener("submit", enterCity);
     let currentDateElement= document.querySelector("#current-date")
